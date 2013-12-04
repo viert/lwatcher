ce33543befdb1593e3401e2997739bef3ff54ae4
Pavel Vorobyov <viert@yandex-team.ru>
2013-12-04 17:58:24 +0400

    command line parser inparse.py

---
+++ src/inparse.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from optparse import OptionParser
+from lwlib.configreader import ConfigReader
+from lwlib.parser import LogParser
+import sys
+
+op = OptionParser()
+op.add_option("-c", "--config", dest="configfile", help="Use config FILE", type="string")
+(options,args) = op.parse_args()
+if options.configfile is None:
+  print "Config filename is missing"
+  print "Use inparse.py -c FILE"
+  sys.exit(1)
+
+c = ConfigReader(options.configfile)
+parser = LogParser(c.config['parser'])
+while True:
+  line = sys.stdin.readline()
+  
+  if line == '':
+    break
+
+  fields = parser.parseLine(line)
+  if parser.successful:
+    print repr(fields)
+  else:
+    print "FAILED TO PARSE"
