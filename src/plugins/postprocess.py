804cb18b4b4cb7df5e3ca268646fad6d2600bd67
Pavel Vorobyov <viert@yandex-team.ru>
2013-12-25 16:55:12 +0400

    new postprocess functions

--- src/plugins/postprocess.py
+++ src/plugins/postprocess.py
@@ -4,11 +4,29 @@ import dateutil.parser
 import datetime
 import logging
 
-def convertDate(record, key, format=None):
+def convertToDatetime(record, key, format=None):
   if format is None:
     dt = dateutil.parser.parse(record[key])
   else:
     dt = datetime.datetime.strptime(record[key], format)
   record[key] = dt
+  
+def convertToFloat(record, key, default=None):
+  try:
+    record[key] = float(record[key])
+  except ValueError:
+    if not default is None:
+      record[key] = default
 
-exports = [convertDate]
+def convertToInt(record, key, default=None, base=10):
+  try:
+    record[key] = int(record[key], base)
+  except ValueError:
+    if not default is None:
+      record[key] = default
+
+# backward compatibility function
+def convertDate(*args):
+  return convertToDatetime(args)
+
+exports = [convertDate, convertToFloat, convertToInt, convertToDatetime]
