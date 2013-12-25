49e03075f5bcff42b77dc7d155a8a2330b52f731
Pavel Vorobyov <viert@yandex-team.ru>
2013-12-25 18:51:42 +0400

    discrete timings function (alpha)

--- src/plugins/accesslog.py
+++ src/plugins/accesslog.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 from collections import Counter
-import datetime
+import datetime, re, logging
+
+COMPILED_RE = {}
 
 def CountRPSbyVhost(store):
   "needs @vhost and @datetime field indexed"
@@ -23,4 +25,31 @@ def Count500byVhost(store):
   "needs @status:@vhost index"
   return store.indexes['status.vhost']['counter']['500']
 
-exports = [Count500byVhost, CountRPSbyVhost]
+def DiscreteTimings(store, url_filter=None):
+  if url_filter in COMPILED_RE:
+    regexp = COMPILED_RE[url_filter]
+  else:
+    try:
+      regexp = re.compile(url_filter)
+      COMPILED_RE[url_filter] = regexp
+    except:
+      logging.error('Invalid RegExp: "%s"' % url_filter)
+      return {}
+  result = Counter()
+  invalid_time = 0
+  count = 0
+  for record in store:
+    count += 1
+    if regexp.search(record['url']):
+      try:
+        timing = int(record['time']*1000)/10
+      except:
+        invalid_time += 1
+      result[timing] += 1
+      
+  if invalid_time:
+    logging.error('Invalid timings found (%d of %d). Are you still store "time" as string?' % (invalid_time, count))
+  return result
+  
+
+exports = [Count500byVhost, CountRPSbyVhost, DiscreteTimings]
